f04de7ee10e543e16fb819a6152945e25c6c2fa9
@seseblante <seseblante@up.edu.ph>
2024-04-09 01:43:40 +0800

    Complete exception handling and error checking

--- src/main/java/org/quinemccluskey/algorithm/MainController.java
+++ src/main/java/org/quinemccluskey/algorithm/MainController.java
@@ -12,21 +12,37 @@ public class MainController {
     private TextArea mtTextArea;
 
     @FXML
+    private TextArea startVariableTextArea;
+
+    @FXML
     private TextArea outputTextArea;
 
     public void solve() {
         String input1 = mtTextArea.getText();
         String input2 = dcTextArea.getText();
+        String input3 = startVariableTextArea.getText();
         // Parse input and perform calculations
         try {
-            Quine_McCluskey qm = new Quine_McCluskey(input1, input2);
+            Quine_McCluskey qm = new Quine_McCluskey(input1, input2, input3);
             qm.solve1();
             outputTextArea.setText(qm.getResultsAsString());
-        } catch (RuntimeException e) {
-            if (isNumber(input1) && isNumber(input2)) {
-                outputTextArea.setText("INVALID INPUT: Your minterms and don't cares have one or more common terms.");
+            if (isNumber(input3)) {
+                outputTextArea.setText("INVALID INPUT: Use letters only.");
             }
-            else outputTextArea.setText("INVALID INPUT: One of your inputs is not a number.");
+        } catch (RuntimeException e) {
+//            if (isNumber(input1) && isNumber(input2)) {
+//                outputTextArea.setText("INVALID INPUT: Your minterms and don't cares have one or more common terms.");
+//            }
+//            else outputTextArea.setText("INVALID INPUT: One of your inputs is not a number.");
+            if (input1.isEmpty()) {
+                outputTextArea.setText("You did not input any minterms.");
+            } else if (input1.isEmpty() && input2.isEmpty()) {
+                outputTextArea.setText("You did not input any minterms.");
+            } else if (!checkIfNumber(input1) && !checkIfNumber(input2)) {
+                outputTextArea.setText("INVALID INPUT: One of your inputs is not a number.");
+            } else if (!checkIfNumber(input1) || !checkIfNumber(input2)) {
+                outputTextArea.setText("INVALID INPUT: One of your inputs is not a number.");
+            } else outputTextArea.setText("INVALID INPUT: Your minterms and don't cares have one or more common terms.");
         }
     }
     public static boolean isNumber(String x) {
@@ -36,6 +52,16 @@ public class MainController {
         } catch (NumberFormatException e) {
             return false;
         }
+    }
+
+    public static boolean checkIfNumber(String str) {
+        String[] numbers = str.split("[,\\s]+");
+        for (String s : numbers) {
+            if (!isNumber(s)) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
