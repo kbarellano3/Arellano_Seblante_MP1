7f2f2dfba272f9a49f8503845fd28c080bb325de
Kristine Joy Arellano <kbarellano3@up.edu.ph>
2024-04-09 17:07:04 +0800

    Set program launch to full screen

--- src/main/java/org/quinemccluskey/algorithm/Main.java
+++ src/main/java/org/quinemccluskey/algorithm/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,8 +14,17 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/org/quinemccluskey.algorithm/Main.fxml"));
-        primaryStage.setTitle("Quine McCluskey Algorithm");
-        primaryStage.setScene(new Scene(root, 600, 400));
+
+        // Set the initial width and height to the maximum available width and height
+        Screen screen = Screen.getPrimary();
+        double maxWidth = screen.getVisualBounds().getWidth();
+        double maxHeight = screen.getVisualBounds().getHeight();
+        primaryStage.setWidth(maxWidth);
+        primaryStage.setHeight(maxHeight);
+
+        // Set the scene and show the primary stage
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
